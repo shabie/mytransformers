82d8b8a404d3911783da61d0bb8d3d87fe2b66b8
Shabie Iqbal <shabieiqbal@gmail.com>
2021-04-04 01:31:27 +0200

    fix example

--- examples/letsdownload.py
+++ examples/letsdownload.py
@@ -1,5 +1,14 @@
 from mytransformers.downloaders import fetch_model
-files_to_save = None
+
+
+# set to None for all files
+files_to_save = [
+    "pytorch_model.bin",
+    "config.json",
+    "tokenizer_config.json",
+    "vocab.json",
+]
+
 url = "https://huggingface.co/ankur310794/roberta-base-squad2-nq"
 
 fetch_model(url, download_dir=".", files_to_save=files_to_save, upload_to_kaggle=True)
